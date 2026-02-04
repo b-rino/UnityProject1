e6bc6b63fc8d6f2ec8ec4d628a921cd7b967f283
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2026-02-04 20:48:48 +0100

    Added mouse rotation to camera and player skin and animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator animator;
     public float moveSpeed = 5f;
+    public float sprintSpeed = 8f;
     public float acceleration = 10f;
 
     private Rigidbody rb;
     private Vector3 inputDirection;
 
+    public Transform playerBase;
+
+    private bool isSprinting;
+
     void Start()
     {
-        // transform.root er PlayerBase
         rb = transform.root.GetComponentInChildren<Rigidbody>();
-      
         rb.freezeRotation = true;
     }
 
@@ -22,13 +26,25 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
 
         inputDirection = new Vector3(h, 0, v).normalized;
+
+        // Sprint = bare hurtigere run
+        isSprinting = Input.GetKey(KeyCode.LeftShift) && inputDirection.magnitude > 0;
+
+        // Animator får kun speed
+        animator.SetFloat("speed", rb.linearVelocity.magnitude);
     }
 
     void FixedUpdate()
     {
-        Vector3 targetVelocity = transform.TransformDirection(inputDirection) * moveSpeed;
+        float currentSpeed = isSprinting ? sprintSpeed : moveSpeed;
 
-        Vector3 velocity = Vector3.Lerp(rb.linearVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+        Vector3 targetVelocity = playerBase.TransformDirection(inputDirection) * currentSpeed;
+
+        Vector3 velocity = Vector3.Lerp(
+            rb.linearVelocity,
+            targetVelocity,
+            acceleration * Time.fixedDeltaTime
+        );
 
         rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
     }
