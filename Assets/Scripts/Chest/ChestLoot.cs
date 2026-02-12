a3166f803cb3f527f550054f117aa07f0e4262eb
Thomas Atchapero <t.atchapero@gmail.com>
2026-02-12 21:27:48 +0100

    Added money counter that increases on looting chests

--- Assets/Scripts/Chest/ChestLoot.cs
+++ Assets/Scripts/Chest/ChestLoot.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ChestLoot : MonoBehaviour
 {
     [SerializeField] private GameObject coins;
+    [SerializeField] private int moneyAmount = 50;
     private bool isLooted = false;
     private bool playerInRange = false;
 
@@ -17,6 +18,14 @@ public class ChestLoot : MonoBehaviour
     void LootChest()
     {
         isLooted = true;
+
+        Debug.Log("[Chest] LootChest. Wallet.Instance is " + (Wallet.Instance == null ? "NULL" : "OK"));
+
+        if (Wallet.Instance != null)
+        {
+            Wallet.Instance.Add(moneyAmount);
+            Debug.Log("Added '" + moneyAmount + "' gold.");
+        }
 
         if (coins != null)
             coins.SetActive(false);
